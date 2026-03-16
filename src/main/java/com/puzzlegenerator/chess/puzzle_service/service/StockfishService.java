fb7430c233ae05d9b30e222078d82260b0cdfa84
Shalmaliiii <79739505+Shalmaliiii@users.noreply.github.com>
2026-03-16 16:39:59 +0530

    Fixed Test Case
    Signed-off-by: Shalmaliiii <79739505+Shalmaliiii@users.noreply.github.com>

--- src/main/java/com/puzzlegenerator/chess/puzzle_service/service/StockfishService.java
+++ src/main/java/com/puzzlegenerator/chess/puzzle_service/service/StockfishService.java
@@ -3,6 +3,7 @@ package com.puzzlegenerator.chess.puzzle_service.service;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.io.*;
 
 @Service
+@ConditionalOnProperty(name = "stockfish.enabled", havingValue = "true", matchIfMissing = true)
 public class StockfishService {
 
     private static final Logger logger =
